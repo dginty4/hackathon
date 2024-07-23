6e6b4de65801afe7b11f329c6f45340ed8406c46
dginty4 <declan.ginty@genesys.com>
2024-07-23 19:06:51 +0100

    Added animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,49 +6,90 @@ public class PlayerController : MonoBehaviour
 {
     // Public variables
     public float moveSpeed = 5f;
-    public float jumpSpeed = 5f;
+    public float jumpPower = 5f;
+    public int jumps = 2;
     
     // Private variables
+    private bool isMoving;
+    private bool isJumping;
     private Rigidbody2D rb;
-    private int jumpCounter = 2;
+    private Animator animator;
+    private SpriteRenderer renderer;
+    private float movement;
+    private bool facingRight;
+    private int jumpCounter;
     
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        renderer = GetComponent<SpriteRenderer>();
+        jumpCounter = jumps;
     }
 
     void Update()
     {
-            // Get input from the player
-            var movement = Input.GetAxisRaw("Horizontal");
-            var targetPos = transform.position;
-            
-            // Move
-            if (movement != 0.0)
-            {
-                targetPos.x += movement;
-                StartCoroutine(Move(targetPos));
-            }
-
-            // Jump
-            if (Input.GetKeyDown(KeyCode.Space) && jumpCounter > 0)
-            {
-                rb.AddForce(new Vector2(rb.velocity.x, jumpSpeed), ForceMode2D.Impulse);
-                jumpCounter--;
-            }
+        // Get input from the player
+        movement = Input.GetAxisRaw("Horizontal");
+        
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            isMoving = true;
+        
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+            isMoving = false;
+        
+        if (Input.GetButtonDown("Jump"))
+            Jump();
+        
+        animator.SetFloat("yVelocity", rb.velocity.y);
+    }
+    
+    void FixedUpdate()
+    {
+        Move(movement);        
+    }
+    
+    void Jump()
+    {
+        if (jumpCounter > 0)
+        {
+            jumpCounter--;
+            rb.velocity = Vector2.up * jumpPower;
+            animator.SetBool("jumping", true);
+        }
     }
 
-    IEnumerator Move(Vector3 targetPos)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        transform.position = Vector2.MoveTowards(transform.position, targetPos, (moveSpeed * Time.deltaTime));
-        yield return null;
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
+        // Landing from jump
         if (collision.gameObject.layer == LayerMask.NameToLayer("SolidObjects"))
         {
             jumpCounter = 2;
+            animator.SetBool("jumping", false);
         }
     }
+
+    void Move(float dir)
+    {
+        float xVal = dir * moveSpeed * 100 * Time.fixedDeltaTime;
+        Vector2 targetVelocity = new Vector2(xVal, rb.velocity.y);
+        rb.velocity = targetVelocity;
+ 
+        //If looking right and clicked left (flip to the left)
+        if(facingRight && dir < 0)
+        {
+            renderer.flipX = true;
+            facingRight = false;
+        }
+        //If looking left and clicked right (flip to rhe right)
+        else if(!facingRight && dir > 0)
+        {
+            renderer.flipX = false;
+            facingRight = true;
+        }
+        
+        animator.SetFloat("xVelocity", Mathf.Abs(rb.velocity.x));
+    }   
+    
+    
 }
