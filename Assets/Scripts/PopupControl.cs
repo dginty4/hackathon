361636d659c48ee0630798a697f456f54d92058f
HemanthDogiparthi12 <hemanth.dogiparthi@genesys.com>
2024-07-24 16:33:42 +0100

    Update scene

--- Assets/Scripts/PopupControl.cs
+++ Assets/Scripts/PopupControl.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-
+using UnityEngine.SceneManagement;
 public class PopupControl : MonoBehaviour
 {
     public GameObject popupPanel;
@@ -28,6 +28,13 @@ public class PopupControl : MonoBehaviour
         popupText.text = message;
         popupPanel.GetComponentInChildren<TMP_Text>().text = buttonMessage;
         popupPanel.SetActive(true);
+
+        if(!isCorrect) {
+             SceneManager.LoadScene("Main_Menu", LoadSceneMode.Single);
+        } else {
+            int nextScene =SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(nextScene);
+        }
     }
 
     public void onAnswerSelected(string message, bool isCorrect) {
